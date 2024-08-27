3bf69420a0da60763598bfaaf518437b2d35bb6b
Ngô Tấn Quốc <Ngotanquocqn@gmail.com>
2024-08-27 17:01:11 +0700

    First commit

---
+++ BaiTapBuoi5/src/baitapbuoi5/Bai2.java
@@ -0,0 +1,18 @@
+package baitapbuoi5;
+
+import java.util.Random;
+
+public class Bai2 {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int r = rand.nextInt(3);
+        if (r == 0) {
+            System.out.println("Rock");
+        } else if (r == 1) {
+            System.out.println("Paper");
+        } else {
+            System.out.println("Scissors");
+        }
+    }
+}
---
+++ BaiTapBuoi5/src/baitapbuoi5/BaiTapBuoi5.java
@@ -0,0 +1,42 @@
+package baitapbuoi5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BaiTapBuoi5 {
+
+    public static void main(String[] args) {
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 10; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+
+       // Scanner sc = new Scanner(System.in);
+//        int sum = 0;
+//
+//        System.out.println("Enter a number (-1 to quit): ");
+//        int number = sc.nextInt();
+//
+//        while (number != -1) {
+//            sum = sum + number;
+//            System.out.println("Enter a number (-1 to quit): ");
+//            number = sc.nextInt();
+//        }
+//        System.out.println("The total is: " + sum);
+
+//            String p;
+//            do{
+//                System.out.println("Nhap tu ban phim: ");
+//                p = sc.next();
+//            }
+//            while(!p.equals("123"));
+            
+        Random sc = new Random();
+        int n = sc.nextInt(10);
+        System.out.println(n);
+            
+    }
+
+}
